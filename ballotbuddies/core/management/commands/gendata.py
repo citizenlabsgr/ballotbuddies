3ea02198b3d713f24bb5a25106bcf9c0f8f7e6b9
Jace Browning <jacebrowning@gmail.com>
2021-10-15 15:09:20 -0400

    Generate test data

--- ballotbuddies/core/management/commands/gendata.py
+++ ballotbuddies/core/management/commands/gendata.py
@@ -1,45 +1,35 @@
-import random
-from contextlib import suppress
-
 from django.conf import settings
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand
 from django.db.utils import IntegrityError
 
-import log
-from faker import Faker
-
-# from ballotbuddies.buddies import models
-
-
-User = get_user_model()
+from ballotbuddies.buddies.models import Voter
 
 
 class Command(BaseCommand):
-    help = "Generate data for automated testing and manual review"
 
-    fake = Faker()
-    new_user_id = None
+    help = "Generate data for automated testing and manual review"
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "emails",
+            "voters",
             nargs="?",
-            type=lambda value: value.split(","),
+            type=lambda value: value.split("/"),
             default=[],
         )
 
-    def handle(self, *, emails, **_options):
+    def handle(self, *, voters, **_options):
         self.update_site()
-        admin = self.get_or_create_superuser()
-        self.new_user_id = self.get_or_create_user("newbie@example.com").id
-        users = [self.get_or_create_user(email) for email in emails]
-        self.generate_review_data(admin, *users)
+        self.get_or_create_superuser()
+        self.get_or_create_user("newbie@example.com")
+        for voter in voters:
+            info = voter.split(",")
+            self.get_or_create_voter(*info)
 
     def update_site(self):
         site = Site.objects.get(id=1)
-        site.name = f"ballotbuddies {settings.BASE_NAME}"
+        site.name = f"Ballot Buddies {settings.BASE_NAME}"
         site.domain = settings.BASE_DOMAIN
         site.save()
         self.stdout.write(f"Updated site: {site}")
@@ -51,14 +41,14 @@ class Command(BaseCommand):
                 email=f"{username}@{settings.BASE_DOMAIN}",
                 password=password,
             )
-            self.stdout.write(f"Created new superuser: {user}")
+            self.stdout.write(f"Created superuser: {user}")
         except IntegrityError:
             user = User.objects.get(username=username)
-            self.stdout.write(f"Found existing superuser: {user}")
+            self.stdout.write(f"Found superuser: {user}")
 
         return user
 
-    def get_or_create_user(self, base_email, password="password"):
+    def get_or_create_user(self, base_email: str, password="password"):
         username, email_domain = base_email.split("@")
 
         user, created = User.objects.get_or_create(username=username)
@@ -67,30 +57,32 @@ class Command(BaseCommand):
         user.save()
 
         if created:
-            self.stdout.write(f"Created new user: {user}")
+            self.stdout.write(f"Created user: {user}")
         else:
-            self.stdout.write(f"Update user: {user}")
+            self.stdout.write(f"Updated user: {user}")
 
         return user
 
-    def generate_review_data(self, *users):
-        count = User.objects.count()
-        while count < 50:
-            with suppress(IntegrityError):
-                user = User.objects.create(
-                    username=self.fake_username(),
-                )
-                self.stdout.write(f"Created user: {user}")
-                count += 1
+    def get_or_create_voter(
+        self,
+        base_email: str,
+        first_name: str,
+        last_name: str,
+        birth_date: str,
+        zip_code: str,
+    ):
+        user = self.get_or_create_user(base_email)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
 
-        # TODO: Create additional models here
-        log.debug(users)
+        voter, created = Voter.objects.update_or_create(
+            user=user, defaults=dict(birth_date=birth_date, zip_code=zip_code)
+        )
 
-    def random_user(self, skip=None):
-        skip_ids = [self.new_user_id]
-        if skip:
-            skip_ids.append(skip.id)
-        return random.choice(User.objects.exclude(id__in=skip_ids))
+        if created:
+            self.stdout.write(f"Created voter: {voter}")
+        else:
+            self.stdout.write(f"Updated voter: {voter}")
 
-    def fake_username(self):
-        return self.fake.name().replace(" ", "").lower()
+        return voter
