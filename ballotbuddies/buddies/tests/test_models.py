161c2eaa7f4ac9d05add71f5c5e2140efa4ebe62
Jace Browning <jacebrowning@gmail.com>
2021-10-18 14:44:36 -0400

    Test that a voter cannot be their own friend

--- ballotbuddies/buddies/tests/test_models.py
+++ ballotbuddies/buddies/tests/test_models.py
@@ -20,3 +20,24 @@ def describe_voter():
 
             expect(updated) == True
             expect(error) == ""
+
+        @pytest.mark.vcr
+        def with_invalid_registration(expect, voter):
+            voter.zip_code = ""
+            voter.status = None
+
+            updated, error = voter.update()
+
+            expect(updated) == False
+            expect(error) == ""
+
+    def describe_save():
+        @pytest.mark.django_db
+        def it_remove_self_from_friends(expect, voter):
+            voter.user.save()
+            voter.save()
+
+            voter.friends.add(voter)
+            voter.save()
+
+            expect(voter.friends.all()).excludes(voter)
