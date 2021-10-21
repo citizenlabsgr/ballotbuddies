a6a4cecff85e919eb9fbab94c5db16eee26ed782
Jace Browning <jacebrowning@gmail.com>
2021-10-21 11:08:13 -0400

    Update domains

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ import dj_database_url
 from .default import *
 
 BASE_NAME = os.environ["HEROKU_APP_NAME"]
-BASE_DOMAIN = f"{BASE_NAME}.com"
+BASE_DOMAIN = "buddies.michiganelections.io"
 BASE_URL = f"https://{BASE_DOMAIN}"
 
 ###############################################################################
@@ -14,11 +14,7 @@ BASE_URL = f"https://{BASE_DOMAIN}"
 
 SECRET_KEY = os.environ["SECRET_KEY"]
 
-ALLOWED_HOSTS = [
-    "0.0.0.0",
-    "localhost",
-    ".herokuapp.com",  # TODO: Remove this line and add your custom domain
-]
+ALLOWED_HOSTS = ["localhost", ".michiganelections.io"]
 
 ###############################################################################
 # Databases
--- config/settings/staging.py
+++ config/settings/staging.py
@@ -6,10 +6,7 @@ BASE_NAME = os.environ["HEROKU_APP_NAME"]
 if BASE_NAME.count("-") >= 2:
     BASE_DOMAIN = f"{BASE_NAME}.herokuapp.com"
 else:
-    BASE_NAME, SUBDOMAIN = BASE_NAME.rsplit("-", 1)
-    BASE_DOMAIN = f"{SUBDOMAIN}.{BASE_NAME}.com"
-    # TODO: Remove this line when the site has it's own domain
-    BASE_DOMAIN = "ballotbuddies-staging.herokuapp.com"
+    BASE_DOMAIN = "staging-buddies.michiganelections.io"
 BASE_URL = f"https://{BASE_DOMAIN}"
 
 
@@ -18,10 +15,7 @@ ALLOW_DEBUG = True
 ###############################################################################
 # Core
 
-ALLOWED_HOSTS += [
-    ".herokuapp.com",
-    # TODO: Add your custom domain
-]
+ALLOWED_HOSTS += [".herokuapp.com", ".michiganelections.io"]
 
 ###############################################################################
 # Authentication
