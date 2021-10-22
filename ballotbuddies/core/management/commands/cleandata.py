955194e1244bf0ade80145ca1e77ee7e1311543f
Jace Browning <jacebrowning@gmail.com>
2021-10-22 14:31:07 -0400

    Generate slugs for voters

--- ballotbuddies/core/management/commands/cleandata.py
+++ ballotbuddies/core/management/commands/cleandata.py
@@ -1,8 +1,14 @@
 from django.core.management.base import BaseCommand
 
+import log
+
+from ballotbuddies.buddies.models import Voter
+
 
 class Command(BaseCommand):
     help = "Clean up existing data"
 
     def handle(self, **_options):
-        pass
+        for voter in Voter.objects.filter(slug=""):
+            log.critical("Generating slug for voter")
+            voter.save()
