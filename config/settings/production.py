1a21f76e199157311f3364d0f0989c5a3217506c
Jace Browning <jacebrowning@gmail.com>
2022-02-19 16:11:33 -0500

    Set CSRF trusted origins

--- config/settings/production.py
+++ config/settings/production.py
@@ -19,6 +19,8 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 
 ALLOWED_HOSTS = ["localhost", ".michiganelections.io"]
 
+CSRF_TRUSTED_ORIGINS = ["https://*.michiganelections.io"]
+
 ###############################################################################
 # Databases
 
