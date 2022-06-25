9e925999161d272299da82118e9fb9e1358810c2
Jace Browning <jacebrowning@gmail.com>
2022-06-24 20:03:26 -0400

    Add str() method for profiles

--- ballotbuddies/alerts/models.py
+++ ballotbuddies/alerts/models.py
@@ -15,6 +15,9 @@ class Profile(models.Model):
     last_viewed = models.DateTimeField(auto_now_add=True)
     last_alerted = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return str(self.voter)
+
     def mark_viewed(self):
         self.last_viewed = timezone.now()
         self.save()
