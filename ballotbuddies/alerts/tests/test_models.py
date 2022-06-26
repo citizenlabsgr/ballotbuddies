77a53a5da35945d8e0c9d9f8c9ebd6e5012a2414
Jace Browning <jacebrowning@gmail.com>
2022-06-26 16:08:04 -0400

    Add a test for updating activity

--- ballotbuddies/alerts/tests/test_models.py
+++ ballotbuddies/alerts/tests/test_models.py
@@ -1,7 +1,20 @@
-# pylint: disable=expression-not-assigned,singleton-comparison,unused-variable
+# pylint: disable=expression-not-assigned,singleton-comparison,unused-variable,redefined-outer-name
 
 
-from ballotbuddies.alerts.models import Profile
+import pytest
+
+from ballotbuddies.alerts.models import Message, Profile
+from ballotbuddies.buddies.models import Voter
+
+
+@pytest.fixture
+def voter(admin_user):
+    return Voter.objects.from_user(admin_user)
+
+
+@pytest.fixture
+def profile(voter):
+    return Profile.objects.create(voter=voter)
 
 
 def describe_profile():
@@ -12,3 +25,14 @@ def describe_profile():
 
         expect(profile.last_viewed_days) == 0
         expect(profile.should_alert) == False
+
+    @pytest.mark.django_db
+    def it_can_update_activity(expect, profile, voter):
+        voter.status = {"message": "Doing stuff"}
+        profile.alert(voter)
+        voter.id = 2
+        voter.status = {"message": "Doing more stuff"}
+        profile.alert(voter)
+
+        message: Message = Message.objects.get_draft(profile)
+        expect(message.body).contains("2 friends")
