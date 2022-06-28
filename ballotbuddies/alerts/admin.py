c5759c4d3aa606711147b7196a5a43bc31f0e961
Jace Browning <jacebrowning@gmail.com>
2022-06-28 17:47:45 -0400

    Hide sent messages in the admin

--- ballotbuddies/alerts/admin.py
+++ ballotbuddies/alerts/admin.py
@@ -1,10 +1,23 @@
 # pylint: disable=unused-argument,no-self-use
 
 from django.contrib import admin, messages
+from django.shortcuts import redirect
 from django.utils.html import format_html
 
 from . import helpers
 from .models import Message, Profile
+
+
+class DefaultQueryMixin(admin.ModelAdmin):
+    def changelist_view(self, request, *args, **kwargs):
+        view = super().changelist_view(request, *args, **kwargs)
+        if request.META["QUERY_STRING"]:
+            return view
+        if request.path in request.META.get("HTTP_REFERER", ""):
+            return view
+        if query := getattr(self, "default_query"):
+            return redirect(request.path + "?" + query)
+        return view
 
 
 @admin.register(Profile)
@@ -44,7 +57,9 @@ def send_selected_messages(modeladmin, request, queryset):
 
 
 @admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
+class MessageAdmin(DefaultQueryMixin, admin.ModelAdmin):
+
+    default_query = "sent__exact=0"
 
     search_fields = [
         "profile__voter__user__email",
