606ddee0bb732be3dae9a6b49aa36b08b311ccc1
Jace Browning <jacebrowning@gmail.com>
2022-07-03 17:04:28 -0400

    Switch to the default Django admin for mobile support

--- ballotbuddies/core/admin.py
+++ ballotbuddies/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+admin.site.site_header = "Ballot Buddies Admin"
+admin.site.site_title = "Ballot Buddies Admin"
 admin.site.index_title = "Home"
+admin.site.enable_nav_sidebar = False
