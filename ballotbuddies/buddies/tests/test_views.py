522c0a1972bcb2fb17f682b7b5f55c4207c2cb44
Jace Browning <jacebrowning@gmail.com>
2022-07-05 20:24:28 -0400

    Add view to search friends by name

--- ballotbuddies/buddies/tests/test_views.py
+++ ballotbuddies/buddies/tests/test_views.py
@@ -2,10 +2,11 @@
 
 from django.utils import timezone
 
+import log
 import pytest
 
 from ..constants import VOTED
-from ..models import Voter
+from ..models import User, Voter
 
 
 @pytest.fixture
@@ -24,12 +25,29 @@ def complete_voter(voter):
     return voter
 
 
+@pytest.fixture
+def friend(voter):
+    user = User.objects.create_user(username="username", password="password")
+    voter2 = Voter.objects.from_user(user, VOTED.status)
+    voter2.nickname = "Friendo"
+    voter2.save()
+    voter.friends.add(voter2)
+    voter.save()
+    return voter2
+
+
+def decode(response) -> str:
+    html = response.content.decode().strip()
+    log.info(f"{response.status_code} response:\n\n{html}\n\n")
+    return html
+
+
 @pytest.mark.django_db
 def describe_index():
     def it_disables_buttons_when_unauthenticated(expect, client, voter):
         response = client.get("/")
 
-        html = response.content.decode()
+        html = decode(response)
         expect(html).excludes("View Profile")
         expect(html.count("disabled")) == 3
 
@@ -38,9 +56,29 @@ def describe_index():
 
         response = client.get(f"/?referrer={voter.slug}")
 
-        html = response.content.decode()
+        html = decode(response)
         expect(html).excludes("View Profile")
         expect(html.count("disabled")) >= 3  # TODO: should be 4 including the voter
+
+
+def describe_friends():
+    def it_can_filter_by_name(expect, client, voter, friend):
+        client.force_login(voter.user)
+
+        response = client.get("/friends/search/")
+        html = decode(response)
+        expect(html.count('id="toggle-')) == 1
+        expect(html).contains(friend.nickname)
+
+        response = client.get("/friends/search/?q=friend")
+        html = decode(response)
+        expect(html.count('id="toggle-')) == 1
+        expect(html).contains(friend.nickname)
+
+        response = client.get("/friends/search/?q=foobar")
+        html = decode(response)
+        expect(html.count('id="toggle-')) == 0
+        expect(html).excludes(friend.nickname)
 
 
 @pytest.mark.django_db
@@ -55,15 +93,15 @@ def describe_profile():
         client.force_login(complete_voter.user)
 
         response = client.get("/profile/")
-        html = response.content.decode()
+        html = decode(response)
         expect(html).contains("checked")
 
         response = client.post("/profile/", follow=True)
-        html = response.content.decode()
+        html = decode(response)
         expect(html).excludes("checked")
 
         response = client.post("/profile/", follow=True)
-        html = response.content.decode()
+        html = decode(response)
         expect(html).contains("checked")
 
 
@@ -79,7 +117,7 @@ def describe_status():
 
         response = client.post(url, {"voted": True})
 
-        html = response.content.decode()
+        html = decode(response)
         expect(html).includes("Didn't vote")
 
     def it_can_manually_clear_voting(expect, client, url, voter):
@@ -87,5 +125,5 @@ def describe_status():
 
         response = client.post(url, {"reset": True})
 
-        html = response.content.decode()
+        html = decode(response)
         expect(html).excludes("Didn't vote")
