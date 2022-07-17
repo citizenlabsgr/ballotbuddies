a7eba1d34024f9da8acde1cd9f59b217d6d074e6
Jace Browning <jacebrowning@gmail.com>
2022-07-17 13:34:51 -0400

    Switch back to a test email domain

--- tests/system/test_site.py
+++ tests/system/test_site.py
@@ -5,6 +5,6 @@ from . import user
 
 def describe_login():
     def it_prompts_to_check_email(expect):
-        user.login("test@gmail.com")
+        user.login("test@example.com")
 
-        expect(user.browser.html).contains("Open gmail.com")
+        expect(user.browser.html).contains("sent to your <b>example.com</b> address")
