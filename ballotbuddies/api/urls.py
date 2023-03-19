7fd966009ead1a3a1a922a15e8eab4a3dff8636e
Jace Browning <jacebrowning@gmail.com>
2023-03-19 15:32:30 -0400

    Add an API to provision voters

--- ballotbuddies/api/urls.py
+++ ballotbuddies/api/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path("provision-voter/", views.provision_voter),
     path("update-ballot/", views.update_ballot),
     path("client/", include("rest_framework.urls")),
 ]
