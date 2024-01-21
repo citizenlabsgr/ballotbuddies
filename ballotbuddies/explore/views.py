8b6c96fe643bae08804b381379aa0e7a5f762eb3
Jace Browning <jacebrowning@gmail.com>
2024-01-21 12:23:44 -0500

    Add a new Django app for ballot exploration

---
+++ ballotbuddies/explore/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpRequest
+from django.shortcuts import render
+
+from asgiref.sync import sync_to_async
+
+from . import helpers
+
+async_render = sync_to_async(render)
+
+
+async def index(request: HttpRequest):
+    proposals = await helpers.get_proposals()
+    context = {"proposals": proposals}
+    return await async_render(request, "explore/index.html", context)
+
+
+async def election_items(request: HttpRequest, election_id: int):
+    election = await helpers.get_election(election_id)
+    proposals = await helpers.get_proposals(election_id=election_id)
+    context = {"election": election, "proposals": proposals}
+    return await async_render(request, "explore/index.html", context)
