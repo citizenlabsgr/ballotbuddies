2f3ca3d32ce54f55a5d2413232c02f326261e40c
Jace Browning <jacebrowning@gmail.com>
2024-01-28 13:30:33 -0500

    Only replace whole words

--- ballotbuddies/core/templatetags/custom_filters.py
+++ ballotbuddies/core/templatetags/custom_filters.py
@@ -1,10 +1,13 @@
+import re
+
 from django import template
 
 register = template.Library()
 
 
 @register.filter
-def replace(value, arg):
-    """Custom template filter to replace string."""
-    search, replace_with = arg.split(",")
-    return value.replace(search, replace_with)
+def replace(text, parameter):
+    """Custom template filter to replace words."""
+    word, replacement = parameter.split(",")
+    pattern = rf"\b{word}\b"
+    return re.sub(pattern, replacement, text)
