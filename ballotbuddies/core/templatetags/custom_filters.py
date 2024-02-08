b786cc4c4e91a4cba51ed20a899e851112410c1a
Jace Browning <jacebrowning@gmail.com>
2024-02-08 11:57:06 -0500

    Fix item count breaking on small screens

--- ballotbuddies/core/templatetags/custom_filters.py
+++ ballotbuddies/core/templatetags/custom_filters.py
@@ -6,8 +6,21 @@ register = template.Library()
 
 
 @register.filter
-def replace(text, parameter):
+def replace(value: str, parameter: str):
     """Custom template filter to replace words."""
     word, replacement = parameter.split(",")
     pattern = rf"\b{word}\b"
-    return re.sub(pattern, replacement, text)
+    return re.sub(pattern, replacement, value)
+
+
+@register.filter
+def abbreviate(value: float):
+    try:
+        value = float(value)
+        if value >= 1000:
+            value = round(value / 1000, 1)
+            return f"{value}k"
+        else:
+            return value
+    except (ValueError, TypeError):
+        return value
