ed3cbfe74418305d5993ac208611382d2df34b90
Jace Browning <jacebrowning@gmail.com>
2024-09-06 18:18:30 -0400

    Lowercase emails

--- ballotbuddies/core/adapters.py
+++ ballotbuddies/core/adapters.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth import login
 from django.shortcuts import redirect
 
+import log
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 from .models import User
@@ -9,11 +10,12 @@ from .models import User
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
-        if sociallogin.is_existing:
-            return None
+        email = sociallogin.user.email.lower()
 
-        if existing_user := User.objects.filter(email=sociallogin.user.email).first():
+        if existing_user := User.objects.filter(email=email).first():
+            log.info(f"Logging in existing social user: {email}")
             login(request, existing_user, backend=settings.AUTHENTICATION_BACKENDS[0])
             return redirect(request.GET.get("next") or "core:index")
 
+        log.info(f"Creating new social user: {email}")
         return super().pre_social_login(request, sociallogin)
