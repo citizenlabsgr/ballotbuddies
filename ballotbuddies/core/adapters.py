bfc2515176b8ef29b89affeaffb6e4f6ee833043
Jace Browning <jacebrowning@gmail.com>
2024-09-07 21:40:17 -0400

    Adjust logic to connect existing user

--- ballotbuddies/core/adapters.py
+++ ballotbuddies/core/adapters.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.contrib.auth import login
 from django.shortcuts import redirect
 
 import log
+from allauth.account.utils import perform_login
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 from .models import User
@@ -10,12 +10,17 @@ from .models import User
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
+        if sociallogin.is_existing:
+            return None
+
         email = sociallogin.user.email.lower()
 
-        if existing_user := User.objects.filter(email=email).first():
-            log.info(f"Logging in existing social user: {email}")
-            login(request, existing_user, backend=settings.AUTHENTICATION_BACKENDS[0])
+        if email and User.objects.filter(email=email).exists():
+            log.info(f"Found existing social account: {email}")
+            user = User.objects.get(email=email)
+            sociallogin.connect(request, user)
+            perform_login(request, user, email_verification="none")
             return redirect(settings.LOGIN_REDIRECT_URL)
 
-        log.info(f"Creating new social user: {email}")
+        log.info(f"Authenticating new social account: {email}")
         return super().pre_social_login(request, sociallogin)
